6424318122a7ef9afd05c99bbe04ed1a5e9e0216
HaBbI4 <karabasov.haba@gmail.com>
2023-01-25 16:09:28 +0300

    Clicker

---
+++ main.py
@@ -0,0 +1,35 @@
+import time
+import threading
+from pynput.mouse import Controller, Button
+from pynput.keyboard import Listener, KeyCode
+# Trigger
+toggle_key = KeyCode(char='s')
+clicking = False
+mouse = Controller()
+
+
+def clicker():
+    """Clicker"""
+    while True:
+        if clicking:
+            mouse.click(Button.left, 1)
+            time.sleep(0.1)
+
+
+def toggle_event(key):
+    """Activity tracking and state switching"""
+    if key == toggle_key:
+        global clicking
+        clicking = not clicking
+
+
+def main():
+    clicking_thread = threading.Thread(target=clicker)
+    clicking_thread.start()
+
+    with Listener(on_press=toggle_event) as listener:
+        listener.join()
+
+
+if __name__ == '__main__':
+    main()
